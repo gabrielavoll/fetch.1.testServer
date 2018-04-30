df28edcd3c01c9cd9c138cdb86be4995229ad0a0
gabby <gabriela.voll@bookspan.com>
2018-04-30 15:53:17 -0400

    set up first api handler

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 1010
+const PORT = process.env.PORT || 3001
 
 express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('visual'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/api', (req, res) => {
+  	res.send({ name: "Heather", age: "40" })
+  })
+  .listen(PORT, () => { console.log(`Listening on ${ PORT }`) })
