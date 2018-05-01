c19aa811c4e0d6aa41019a8a5012dab5e87f59b9
gabby <gabriela.voll@bookspan.com>
2018-05-01 12:04:33 -0400

    addding handling fot testing if body is going threw

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require("body-parser");
+const bodyParserJSON = bodyParser.json();
 const cookieParser = require('cookie-parser');
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 function cookieCreator( x, req, res){
 	var cookieVal = parseInt(req['cookies'][x]);
 	if(typeof cookieVal !== "number" || isNaN(cookieVal) || cookieVal > 99) cookieVal = 1;
 	else cookieVal++;
 	res.cookie(x, cookieVal, { maxAge: 1000 * 60 * 60 * 24, httpOnly: false })
-	res.send()
+  if(req.body) res.send(req.body);
+	else res.send();
 }
 
 express()
@@ -18,8 +21,8 @@ express()
   .use(cookieParser())
   .get('/api/do-nothing', (req, res) => res.send())
   .get('/api', (req, res) => cookieCreator('getCookie', req, res))
-  .put('/api', (req, res) => cookieCreator('putCookie', req, res))
-  .post('/api', (req, res) => cookieCreator('postCookie', req, res))
+  .put('/api', bodyParserJSON, (req, res) => cookieCreator('putCookie', req, res))
+  .post('/api', bodyParserJSON, (req, res) => cookieCreator('postCookie', req, res))
   .listen(PORT, () => {
   	console.log(`Listening on ${ PORT }`)
   });
